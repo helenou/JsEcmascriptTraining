9b7478633f2a28120e33045eaacc75ac78673e45
helenou <ln.lejeune@gmail.com>
2019-11-26 16:24:51 +0100

    adding methods and export+import

--- class/index.js
+++ class/index.js
@@ -1,3 +1,15 @@
-import { foo } from "./util.js";
+import { foo, array, month, bar, baz as Quux } from "./util.js";
 
 console.log(foo());
+
+for(let value of array) {
+
+  console.log(value);
+}
+
+console.log(month);
+
+console.log(bar());
+
+
+console.log(Quux());
--- class/util.js
+++ class/util.js
@@ -4,5 +4,17 @@ function foo(){
 }
 
 const array = ['foo', 'bar', 'baz'];
+const data = ['Jan', 'Feb', 'Mar'];
 
-export { foo, array }
+
+export function bar(){
+  return "Utils::bar()";
+}
+
+export function baz(){
+  return "Utils::baz()";
+}
+
+export { foo, array, data as month }
+
+
